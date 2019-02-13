612ec7ec5c22ad3bdce37d83022c8212ca7930df
Wonjun <wonjune12@hotmail.com>
2019-02-13 17:49:39 -0500

    datepicker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,9 @@
 class HomeController < ApplicationController
-<<<<<<< HEAD
-  def index 
-  end
-  
-=======
+
     def index
         @user = current_user
     end
-<<<<<<< HEAD
 
 
 
-
->>>>>>> origin
-=======
->>>>>>> master
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
+ActiveRecord::Schema.define(version: 2019_02_13_210610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "movies_associations", force: :cascade do |t|
     t.integer "movie_id"
     t.date "setdate"
