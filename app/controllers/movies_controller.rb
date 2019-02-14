b35a0c61bf9de22c2e5ed32e30b9510549e86cec
Ryan Robinson <ryanrobinson@Ryans-MacBook-Pro.local>
2019-02-14 12:06:11 -0500

    api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,15 +2,11 @@ class MoviesController < ApplicationController
     require 'httparty'
     
     def index
-        @response = HTTParty.get('http://www.omdbapi.com/?t='+ params[:search].to_s + '&apikey='ENV["MOVIEVERSE_API_KEY"])
-        # @title = response.title
+        @response = HTTParty.get('http://www.omdbapi.com/?t='+ params[:search].to_s + "&apikey=#{ ENV['MOVIEVERSE_API_KEY'] }")
+       
     end
-    
-    # def create
-    #     @response = HTTParty.get('http://www.omdbapi.com/?t='+ params[:movie].to_s + '&apikey=d6605073')
-    #     redirect_to movies_path
-    # end
-
+     
+ 
     def search
         @respones= Respone.all
     end
