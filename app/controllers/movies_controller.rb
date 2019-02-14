b9d3ef3e1c89f0a8e62de3f74db5c8a4b29f50b2
Ryan Robinson <ryanrobinson@Ryans-MacBook-Pro.local>
2019-02-14 10:15:49 -0500

    form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,13 +2,16 @@ class MoviesController < ApplicationController
     require 'httparty'
     
     def index
-        puts @response = HTTParty.get('http://www.omdbapi.com/?t='+ :userinput.to_s + '&apikey=d6605073')
+        @response = HTTParty.get('http://www.omdbapi.com/?t='+ params[:search].to_s + '&apikey=d6605073')
         # @title = response.title
     end
     
+    # def create
+    #     @response = HTTParty.get('http://www.omdbapi.com/?t='+ params[:movie].to_s + '&apikey=d6605073')
+    #     redirect_to movies_path
+    # end
 
-
-    def show
-        @respones = Respone.all
+    def search
+        @respones= Respone.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/movies' => 'movies#index', as: 'movies'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
   get '/movies/:id' => 'movies#show', as: 'movie'
+ 
 end
