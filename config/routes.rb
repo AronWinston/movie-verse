b4041fab6529de633f7217f650aa678121426721
Wonjun <wonjune12@hotmail.com>
2019-02-15 10:37:42 -0500

    Deleted movieassociation routs and related files

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,6 @@ Rails.application.routes.draw do
   get '/movies/:id' => 'movies#show', as: 'movie'
   get '/search/' => 'movies#search', as: 'search_page'
   resources :movies
-  get 'movies_association/index'
-  get 'movies_association/show'
-  get 'movies_association/edit'
-  get 'movies_association/new'
-  get 'movies_association/create'
-  get 'movies_association/update'
-  get 'movies_association/destroy'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "home#index"
 end
