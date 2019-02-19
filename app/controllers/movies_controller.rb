1b01a924a01ffd7ade6366d05e48900fe97f4003
Ryan Robinson <ryanrobinson@Ryans-MacBook-Pro.local>
2019-02-19 10:49:45 -0500

    ryan

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
     
     def index
 
-        @response = HTTParty.get('http://www.omdbapi.com/?s='+ params[movie_title].to_s + "&apikey=" + ENV['MOVIEVERSE_API_KEY'])
+        @response = HTTParty.get('http://www.omdbapi.com/?s='+ params[:movie_title].to_s + "&apikey=" + ENV['MOVIEVERSE_API_KEY'])
         @user=current_user
         @currentUser = current_user.id
     end
