11d3bc34df494e8fe25d0c241904a642d4f4fa6e
Aron Winston <aronwinston2792@gmail.com>
2019-02-19 15:07:54 -0500

    debugging

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -48,13 +48,7 @@ class MoviesController < ApplicationController
             movietitle: @response["Title"],
             movieposter: @response["Poster"]
         )
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> e93d8a6cb99686ab61e85614699c5485f36076dd
-=======
->>>>>>> e817a4ea35541111ff7a770085e93f464d2ae046
             redirect_to root_path
     end
 
