c8dd45e6f1fc545a7ae1bc119ff5af7d702b571a
Wonjun <wonjune12@hotmail.com>
2019-02-19 13:42:17 -0500

    Removed unecessary lines of codes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,7 +39,6 @@ class MoviesController < ApplicationController
             movietitle: @response["Title"],
             movieposter: @response["Poster"]
         )
-
             redirect_to root_path
     end
 
